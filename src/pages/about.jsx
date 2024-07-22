882fa55df1bab392ffff85b2edb681c92504407d
unknown <dpippin09@gmail.com>
2024-07-22 17:48:39 -0600

    finished the about, contact, and not found files

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import "../Styles/About.css"; // Ensure this is the correct path for your CSS file
+import BrennanImage from "../assets/BrennanWaterbury.png"; // Update with the correct path
+import { TypeAnimation } from "react-type-animation";
 
-function About() {
+export default function About() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>
-        
-      </p>
-      <p>
-        
-      </p>
-      <p>
-        
-      </p>
+    <div className="about">
+      <div className="about-img">
+        <img src={BrennanImage} alt="Brennan Waterbury" />
+      </div>
+      <div className="about-description">
+        <h1>
+        <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Brennan Louis Waterbury',
+        2000, // Wait for 1 second for text to be animated
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '1em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+    </h1>
+        <h2>
+          Full Stack Web Development | Responsive Design
+        </h2>
+        <p>
+          Currently enrolled in a coding bootcamp at the University of Utah, I
+          am actively honing my skills to become a Full-Stack Web Developer,
+          specializing in JavaScript, React, and Node.js. I thrive in
+          environments that foster growth and learning, and I am dedicated to
+          crafting responsive and dynamic websites that make a tangible impact
+          in the tech industry. I am a team player who is coachable and eager to
+          grow in collaborative team environments
+        </p>
+      </div>
     </div>
   );
 }
-
-export default About;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,70 +1,48 @@
-import React, { useState } from 'react';
+import "../Styles/Contact.css";
+import { useForm } from "../components/Form";
 
-function Contact() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    console.log(formData);
-    alert('Thank you for your message. We will get back to you soon!');
-  };
+export default function Contact() {
+  const { formData, errorMessages, handleInputChange, handleInputBlur, handleFormSubmit } = useForm();
 
   return (
-    <div>
-      <h1>Contact Us</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Message:
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <button type="submit">Send</button>
-      </form>
-      <section>
-        <h2>Other Ways to Contact Us</h2>
-        <p>Email: your.email@example.com</p>
-        <p>LinkedIn: yourlinkedinprofile</p>
+    <div className="contact-container text-center">
+      <h1>Hello {formData.name || "Friend"}!</h1>
+      <form className="contact-form" onSubmit={handleFormSubmit}>
+        <input
+          className="contact-input"
+          value={formData.email}
+          name="email"
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          type="email"
+          placeholder="Email"
+        />
+        {errorMessages.email && <p className="error-text">{errorMessages.email}</p>}
         
-      </section>
+        <input
+          className="contact-input"
+          value={formData.name}
+          name="name"
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          type="text"
+          placeholder="Name"
+        />
+        {errorMessages.name && <p className="error-text">{errorMessages.name}</p>}
+        
+        <textarea
+          className="contact-textarea"
+          value={formData.message}
+          name="message"
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          placeholder="Message"
+          rows="5"
+        />
+        {errorMessages.message && <p className="error-text">{errorMessages.message}</p>}
+        
+        <button className="contact-button" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
-
-export default Contact;
