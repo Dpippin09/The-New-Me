79fdef33c24330b6daec93d26738d9c8f7135c2e
unknown <dpippin09@gmail.com>
2024-07-23 17:40:50 -0600

    site works and customized css

---
+++ src/components/pdfComp.jsx
@@ -0,0 +1,41 @@
+// Import PDFComp styles from PDFComp.css.
+// import "../styles/PDFComp.css";
+// Import the Brennan PDF file.
+import David from "../assets/myResume.pdf";
+// Import the useState hook from React.
+import { useState } from "react";
+// Import the pdfjs, Document, and Page components from react-pdf.
+import { pdfjs, Document, Page } from "react-pdf";
+// Set the workerSrc for the pdfjs library.
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+// Export the PDFComponent to be used in the Resume component.
+export default function PDFComponent() {
+  const [numPages, setNumPages] = useState();
+  // Create a function to set the number of pages in the PDF.
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  return (
+    <div className="pdf-container">
+      <Document file={David} onLoadSuccess={onDocumentLoadSuccess}>
+        {/* Create an array of pages to display the PDF. */}
+        {Array.apply(null, Array(numPages))
+          .map((_, i) => i + 1)
+          .map((page) => {
+            return (
+              <Page
+                key={page}
+                pageNumber={page}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                className="pdf-page"
+              />
+            );
+          })}
+      </Document>
+    </div>
+  );
+}
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,28 +1,25 @@
 import "../styles/resume.css";
 import Skills from "../components/skills";
+import PDFComponent from "../components/pdfComp";
 
 export default function Resume() {
   return (
     <div className="resume-container">
-      <div className="resume-download">
+      <div
+       className="resume-download">
+
+       </div>
+    
         <a 
-          href="../assets/" 
-          download=""
+          href="/David's Resume.pdf" 
+          download="David's Resume.pdf"
           className="resume-link"
         >
           Download Resume
         </a>
-      </div>
+      
       <Skills />
-      <div className="resume-viewer">
-        <iframe
-          src="../assets/"
-          width="100%"
-          height="600px"
-          title="Resume"
-        >
-        </iframe>
-      </div>
-    </div>
-  );
+      <PDFComponent />  
+        </div>
+  )
 }
