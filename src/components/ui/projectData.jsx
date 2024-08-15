db547928971e98ada876ae39e07edccbe749dd9c
unknown <dpippin09@gmail.com>
2024-08-15 12:02:09 -0600

    fixing portfolio page

--- src/components/ui/projectData.jsx
+++ src/components/ui/projectData.jsx
@@ -1,19 +1,27 @@
+import tweetcode from "./assets/tweetcode.png";
+import ActivePeakImage from "./assets/ActivePeak.png";
+import PWAImage from "./assets/PWA.png";
+import SocialAPIImage from "./assets/SocialAPI.png";
+import EcomMatrixImage from "./assets/EcomMatrix.png";
+import HRTrackSuiteImage from "./assets/HRTrackSuite.png";
+
+
 const projectsData = [
     {
-      title: "Project 1",
-      image: "https://via.placeholder.com/150",
+      title: "Active Peak",
+      image: ActivePeakImage,
       repoLink: "https://github.com/Dpippin09/Active-Peak",
       liveLink: "active-peak2.onrender.com/"
     },
     {
-      title: "Project 2",
-      image: "https://via.placeholder.com/150",
+      title: "Edit My Text",
+      image: EditMyTextImage,
       repoLink: "https://github.com/Dpippin09/Edit-my-Text",
       liveLink: "#"
     },
     {
-      title: "Project 3",
-      image: "https://via.placeholder.com/150",
+      title: "Tweet Code",
+      image: tweetcode,
       repoLink: " https://github.com/bwater47/Tweet-Code",
       liveLink: " https://tweet-code-ytyj.onrender.com/"
     },
