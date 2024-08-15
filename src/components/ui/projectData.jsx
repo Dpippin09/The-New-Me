5b6f6bcbe62d4c5fd166d602afabea05b8daccb8
unknown <dpippin09@gmail.com>
2024-08-15 12:28:38 -0600

    worked on the projectData.jsx file

--- src/components/ui/projectData.jsx
+++ src/components/ui/projectData.jsx
@@ -1,9 +1,7 @@
-import tweetcode from "../../../assets/tweetcode.png";
-import ActivePeakImage from "./assets/ActivePeak.png";
-import PWAImage from "./assets/PWA.png";
-import SocialAPIImage from "./assets/SocialAPI.png";
-import EcomMatrixImage from "./assets/EcomMatrix.png";
-import HRTrackSuiteImage from "./assets/HRTrackSuite.png";
+import tweetcode from "./assets/tweetcode.png";
+import ActivePeak from "./assets/ActivePeak.png";
+import PWA from "./assets/PWA.png";
+
 
 
 const projectsData = [
@@ -13,36 +11,36 @@ const projectsData = [
     repoLink: " https://github.com/bwater47/Tweet-Code",
     liveLink: " https://tweet-code-ytyj.onrender.com/"
   },
-    {
-      title: "Active Peak",
-      image: ActivePeakImage,
-      repoLink: "https://github.com/Dpippin09/Active-Peak",
-      liveLink: "active-peak2.onrender.com/"
-    },
-    {
-      title: "Edit My Text",
-      image: EditMyTextImage,
-      repoLink: "https://github.com/Dpippin09/Edit-my-Text",
-      liveLink: "#"
-    },
-    {
-      title: "Project 4",
-      image: "https://via.placeholder.com/150",
-      repoLink: "#",
-      liveLink: "#"
-    },
-    {
-      title: "Project 5",
-      image: "https://via.placeholder.com/150",
-      repoLink: "#",
-      liveLink: "#"
-    },
-    {
-      title: "Project 6",
-      image: "https://via.placeholder.com/150",
-      repoLink: "#",
-      liveLink: "#"
-    }
-  ];
-  
-  export default projectsData;
+  {
+    title: "Active Peak",
+    image: ActivePeak,
+    repoLink: "https://github.com/Dpippin09/Active-Peak",
+    liveLink: "active-peak2.onrender.com/"
+  },
+  {
+    title: "PWA",
+    image: PWA,
+    repoLink: "https://github.com/Dpippin09/Edit-my-Text",
+    liveLink: "#"
+  },
+  {
+    title: "Project 4",
+    image: "https://via.placeholder.com/150",
+    repoLink: "#",
+    liveLink: "#"
+  },
+  {
+    title: "Project 5",
+    image: "https://via.placeholder.com/150",
+    repoLink: "#",
+    liveLink: "#"
+  },
+  {
+    title: "Project 6",
+    image: "https://via.placeholder.com/150",
+    repoLink: "#",
+    liveLink: "#"
+  }
+];
+
+export default projectsData;
