02a298d0e456fbd0779160c6b63204192f26408c
unknown <dpippin09@gmail.com>
2024-09-06 17:30:14 -0600

    resume fixes

--- src/components/ui/downloadResume.jsx
+++ src/components/ui/downloadResume.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../../src/styles/resume.css'; // Import the CSS file
+import '../../../public';
 
 const DownloadResume = () => {
   return (
     <div className="resume-container">
       <div className="resume-download">
-        <a href="/resume.pdf" download="My_Resume.pdf" className="resume-link">
+        <a href="/DavidPippinResume.pdf" className="resume-link">
           Download Resume
         </a>
       </div>
